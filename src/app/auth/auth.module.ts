51bc3215dac2e0302a9d938af9d65ee9fc0ddfb9
oldguard149 <tvthuan1409@gmail.com>
2021-06-07 15:47:37 +0700

    Add auth module and shared module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+
+@NgModule({
+  declarations: [
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+  ],
+  imports: [CommonModule, AuthRoutingModule],
+})
+export class AuthModule {}
