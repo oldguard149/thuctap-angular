e8a17aa4d0247bb37b5960cb0ad941f943942b3d
oldguard149 <tvthuan1409@gmail.com>
2021-07-01 09:45:53 +0700

    Add cart/components/checkout ui

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
 
 import { CartRoutingModule } from './cart-routing.module';
 import { CartComponent } from './components/cart/cart.component';
@@ -21,7 +23,9 @@ import { ProductModule } from '../product/product.module';
     SharedModule,
     ReactiveFormsModule,
     ProductModule,
-    NzToolTipModule
+    NzToolTipModule,
+    NzCollapseModule,
+    NzBadgeModule
   ]
 })
 export class CartModule { }
--- src/app/cart/components/checkout/checkout.component.ts
+++ src/app/cart/components/checkout/checkout.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { selectTotalPrice, selectCartItems } from '../../state/cart.selectors';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
+  cartItems$ = this.store.select(selectCartItems);
+  totalPrice$ = this.store.select(selectTotalPrice);
+  vm$ = combineLatest([this.cartItems$, this.totalPrice$]).pipe(
+    map(([cartItems, totalPrice]) => ({ cartItems, totalPrice }))
+  );
 
-  constructor() { }
+  checkoutForm: FormGroup = this.fb.group({
+    name: [''],
+    email: [''],
+    phone: [''],
+    postcode: [''],
+    address: [''],
+    card_tok: ['tok_visa'],
+    description: [''],
+    products: this.fb.array([]),
+  });
 
+  submit() {
+    console.log(this.checkoutForm.value);
+  }
+
+  constructor(private fb: FormBuilder, private store: Store) {}
+  private collapseHeaderSubject = new BehaviorSubject('Show shopping cart');
+  collapseHeader$ = this.collapseHeaderSubject.asObservable();
   ngOnInit(): void {
   }
 
+  changeCollapePanelHeader(value: boolean) {
+    if (value) {
+      this.collapseHeaderSubject.next('Hide shopping cart');
+    } else {
+      this.collapseHeaderSubject.next('Show shopping cart');
+    }
+  }
 }
